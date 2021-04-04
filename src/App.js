1e400c2785aa65068e34fd282088da79d37633ec
Axel Hamilcaro <68954111+axelhamil@users.noreply.github.com>
2021-04-04 18:52:03 +0200

    Quest 3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from "react";
-import Contact from "./components/Contact";
+import ContactList from "./components/ContactList";
 
 function App() {
   return (
     <div className="App">
-      <Contact
-        name="Clara Hopkins"
-        online={true}
-        avatar="https://randomuser.me/api/portraits/women/23.jpg"
-      />
-      <Contact
-        name="Jeremy Harris"
-        online={false}
-        avatar="https://randomuser.me/api/portraits/men/54.jpg"
-      />
-      <Contact
-        name="Larry Hale"
-        online={true}
-        avatar="https://randomuser.me/api/portraits/men/30.jpg"
-      />
+      <ContactList />
     </div>
   );
 }
